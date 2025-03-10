f6c6a4d38bd8dddee8e0d51d051908b62a1930ac
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-03-11 00:58:26 +0100

    patches

--- server/routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { updateProfilePicture } from '../controllers/authController.js';
-import authenticate from '../middlewares/profile.js';
-
-const router = express.Router();
-
-router.post('/updateprofilepicture', authenticate, updateProfilePicture);
-
-export default router;
